75ba0ecc3e6c264be7b0805eb69a5f0a7f7138a2
Christopher Garcia <chrisgarcia0903@gmail.com>
2021-10-31 20:32:59 -0400

    added api to javascript and console logs data corresponding to city the user searches for

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const submitBtn = $('#submit-btn')
-var city = $('#city-search')
-var temp = $('#temperature')
-var humidity = $('#humid')
-var wind = $('#current-wind')
-var uv = $('#uv-index')
+const submitBtn = $("#submit-btn");
+var city = $("#city-search");
+var temp = $("#temperature");
+var humidity = $("#humid");
+var wind = $("#current-wind");
+var uv = $("#uv-index");
 
-submitBtn.on('click', function cityInfo(){
-    console.log(city.val())
-    console.log('hello world')
-})
+submitBtn.on("click", function cityInfo() {
+  cityInput = city.val();
+  console.log(cityInput);
+
+  var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=efce59023ac17e5d9f64c63306a72223`;
+
+  fetch(requestUrl)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+});
