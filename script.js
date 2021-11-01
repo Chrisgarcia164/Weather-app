95f1afdd4c750da2bf2b37e85819c76f3a63aef7
Christopher Garcia <chrisgarcia0903@gmail.com>
2021-10-31 21:05:41 -0400

    gets the lat and lon from the city that the user searches for and uses that to get the five day forecast

--- script.js
+++ script.js
@@ -11,18 +11,32 @@ submitBtn.on("click", function cityInfo() {
   var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=efce59023ac17e5d9f64c63306a72223`;
 
   fetch(requestUrl)
-  .then((response) => {
-    return response.json();
-  })
-  .then(function (data){
-      console.log(data)
-      var temperature = data.main.temp
-      var windSpeed = data.wind.speed 
-      var humid = data.main.humidity
-      temp.text(temperature + '°F')
-      humidity.text(humid + ' %')
-      wind.text(windSpeed + ' MPH')
-  }
-  )
-  
+    .then((response) => {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var temperature = data.main.temp;
+      var windSpeed = data.wind.speed;
+      var humid = data.main.humidity;
+      temp.text(temperature + "°F");
+      humidity.text(humid + " %");
+      wind.text(windSpeed + " MPH");
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      console.log(lon);
+      console.log(lat);
+      fiveDay();
+      function fiveDay() {
+        var fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=f58e0adad5d2bd441a1a32ca9f7c08a4`;
+        fetch(fiveDayUrl)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            console.log(data);
+          });
+      }
+    });
 });
+
